4c507b690005a09fe8534ba4e1d698c0b90fe005
prithmoid <prithmoid@gmail.com>
2024-12-22 17:20:07 +0500

    ref (guesssing_game): add get_guess fn

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,27 +6,37 @@ fn main() {
     let mut secret_number;
     let mut level = 1;
 
+    fn get_guess() -> Option<u32> {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read user input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                return None;
+            }
+        };
+
+        return Some(guess);
+    }
+
     loop {
         secret_number = rand::thread_rng().gen_range(1..=100);
 
         println!("Level {level}, try to guess number:");
 
         loop {
-            let mut guess = String::new();
+            let guess = get_guess();
 
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read user input");
+            if !guess.is_some() {
+                continue;
+            }
 
-            let guess: u32 = match guess.trim().parse() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Please enter a valid number!");
-                    continue;
-                }
-            };
-
-            match guess.cmp(&secret_number) {
+            match guess.unwrap().cmp(&secret_number) {
                 Ordering::Less => println!("Too small!"),
                 Ordering::Greater => println!("Too big!"),
                 Ordering::Equal => {
